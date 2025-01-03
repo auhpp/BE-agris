43f01c08221ba4c1ac79bc81f959de4e65ea83b6
auhpp <hpphiau@gmail.com>
2025-01-03 09:31:32 +0700

    AGS-3 init project

---
+++ src/main/java/com/agri_supplies_shop/AgriSuppliesShopApplication.java
@@ -0,0 +1,13 @@
+package com.agri_supplies_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriSuppliesShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriSuppliesShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agri_supplies_shop/AgriSuppliesShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agri_supplies_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriSuppliesShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
