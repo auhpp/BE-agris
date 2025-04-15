d98f02e8349a971ba6a1608555532a00c818a1e1
auhpp <hpphiau@gmail.com>
2025-04-15 19:51:00 +0700

    AGS-48 build calculation unit api

--- src/main/java/com/agri_supplies_shop/service/CalculationUnitService.java
+++ src/main/java/com/agri_supplies_shop/service/CalculationUnitService.java
@@ -2,6 +2,7 @@ package com.agri_supplies_shop.service;
 
 import com.agri_supplies_shop.dto.request.CalculationUnitRequest;
 import com.agri_supplies_shop.dto.response.CalculationUnitResponse;
+import com.agri_supplies_shop.dto.response.PageResponse;
 
 import java.util.List;
 
@@ -9,4 +10,8 @@ public interface CalculationUnitService {
     CalculationUnitResponse create(CalculationUnitRequest request);
 
     List<CalculationUnitResponse> getAll();
+
+    PageResponse<CalculationUnitResponse> search(String name, int page, int size);
+
+    void delete(Long id);
 }
